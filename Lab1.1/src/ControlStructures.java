c85e3a0ed6eef439204374dd78117a7a61fb10cb
huang <huang@DESKTOP-LDKDTR6.home>
2017-09-30 22:53:22 -0400

    finished!

--- Lab1.1/src/ControlStructures.java
+++ Lab1.1/src/ControlStructures.java
@@ -2,7 +2,8 @@
 
 public class ControlStructures 
 {
-
+	
+	
 	public static void main(String [] args)
 	{
 		fooBarBaz();
@@ -10,11 +11,45 @@ public class ControlStructures
 		System.out.print("finished!");
 	}
 		
+	
+		public static boolean isPrime(int number )
+		{
+			for(int x=2; 2+x<=number; x++)
+			{
+				if(number%x==0)
+					{return false;}
+			}
+		
+					{return true;}
+		
+		}
+
+		
+		public static void primePrinter()
+		{
+			int x =2;
+			for (int y= 0; y <50;)
+			{ 
+				if (isPrime(x))
+				{
+					System.out.print((x+ " "));
+					y++;
+					if (y %5 ==0)
+					{
+						System.out.println();
+					}
+				}
+				x++;
+			}
+		
+		}
+		
+
 		public static void fooBarBaz()
 		{
 			for(int x=1; x<=500; x++)
 			
-			{	if (x%3==0)
+			{	if (x%3==0) 
 					System.out.print("foo");
 				
 				if (x%7==0)
@@ -26,33 +61,8 @@ public class ControlStructures
 				if ((x%3!=0) && (x%7!=0) && (x%10!=0))
 					System.out.print(x);
 					
-				System.out.println("");
+				System.out.println();
 				
 			}
-		}
-		
-		
-		public static void primePrinter()
-		{
-			isPrime();
-		}
-
-		
-			
-			
-		}
-		
-
-		public static boolean isPrime(int number )
-		{
-			
-			for(int x=2; 2+x<number; x++)
-			
-			{	if(number%x==0)
-					{return false;}
-			}
-			
-			{return true;}  
-			
 		}
 }
